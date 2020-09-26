9c33e9734296a13b12066dce6049c990ab0179c2
pablo <26plau26@gmail.com>
2020-09-25 19:34:38 -0500

    reviving functionality

--- game.py
+++ game.py
@@ -23,14 +23,17 @@ class Game:
 
         self.duck_x = 400
         self.duck_y = 400
-        self.duck_vel = 1
+        self.duck_vel = 2
         self.duck_mask = pygame.mask.from_surface(self.duck)
         self.duck_direction = "sideways"
 
         self.counter = 0
         self.counter2 = 0
         self.rotate_counter = 0
-        self.rotare_main = 90
+        self.rotate_main = 90
+
+        self.game_active = True
+        self.score = 0
         
         self.mainloop()
 
@@ -40,7 +43,26 @@ class Game:
         return mask1.overlap(mask2, (offsetx, offsety)) != None
 
     def endscreen(self):
-        pass
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_r]:
+            self.bar = pygame.transform.scale(pygame.image.load("assets/bar.png"), (200,200)).convert_alpha()
+            self.bar_x = 600
+            self.bar_y = 300
+            self.bar_mask = pygame.mask.from_surface(self.bar)
+            self.bar_direction = "up" 
+            self.duck_direction = "sideways"
+            self.bar_direction_counter = 0
+            self.duck_x = 400
+            self.duck_y = 400
+            self.duck_vel = 2
+            self.score = 0
+            self.game_active = True
+        dead_font = pygame.font.SysFont("comicsans", 70)
+        dead_label = dead_font.render("You lost with a score of: " + str(self.score), 1, (255,255,255))
+        restart_font = pygame.font.SysFont("comicsans", 70)
+        restart_label = restart_font.render("To restart press 'r'", 1, (255,255,255))
+        self.screen.blit(dead_label, (800/2 - dead_label.get_width()/2, 800/2 - dead_label.get_height()/2))
+        self.screen.blit(restart_label, (800/2 - restart_label.get_width()/2, (800/2 - restart_label.get_height()/2) + 100))
 
     def play(self):
         keys = pygame.key.get_pressed()
@@ -73,15 +95,15 @@ class Game:
             if self.bar_direction == "up":
                 self.duck_direction = "sideways"
                 if self.bar_x >= self.duck_x - 8:
-                    self.duck_vel = -1
+                    self.duck_vel = -2
                 elif self.bar_x <= self.duck_x:
-                    self.duck_vel = 1
+                    self.duck_vel = 2
             elif self.bar_direction == "sideways":
                 self.duck_direction = "up"
                 if self.bar_y + 100 >= self.duck_y:
-                    self.duck_vel = -1
+                    self.duck_vel = -2
                 elif self.bar_y <= self.duck_y:
-                    self.duck_vel = 1
+                    self.duck_vel = 2
 
         if self.duck_direction == "sideways":
             self.duck_x += self.duck_vel
@@ -90,9 +112,15 @@ class Game:
 
         self.screen.blit(self.duck,(self.duck_x,self.duck_y))
         self.screen.blit(self.bar,(self.bar_x,self.bar_y))
+
+        if self.duck_x >= 800 or self.duck_x <= 0 or self.duck_y >= 800 or self.duck_y <= 0:
+            self.game_active = False
  
     def main(self):
-        self.play()
+        if self.game_active:
+            self.play()
+        else:
+            self.endscreen()
 
     def mainloop(self):
         while True:
